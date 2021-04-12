9ec02f2b27f7189e3e1582c5fd815acb8d300215
Maria Grebenkova <grebenkova.m.p@gmail.com>
2021-04-12 23:27:42 +0300

    fix typo in path to chromedriver

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -16,7 +16,7 @@ public class OrderTest {
 
     @BeforeAll
         static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "\\driver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/driver/chromedriver.exe");
     }
 
     @BeforeEach
