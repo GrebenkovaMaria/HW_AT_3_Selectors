64747e463dc7f0fb156a88ca4bdab8fd51972b24
Maria Grebenkova <grebenkova.m.p@gmail.com>
2021-04-13 01:21:12 +0300

    trying to config webdrivermanager to linux for ubuntu in yml

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -19,9 +19,9 @@ public class OrderTest {
 
     @BeforeAll
         static void setUpAll() {
+        WebDriverManager.chromedriver().config().setOs("Linux");
         WebDriverManager.chromedriver().setup();
-        WebDriverManager.chromedriver().config().setOs("Ubuntu");
-       // System.setProperty("webdriver.chrome.driver", "HW_AT_3_Selectors/driver/chromedriver.exe");
+        // System.setProperty("webdriver.chrome.driver", "HW_AT_3_Selectors/driver/chromedriver.exe");
     }
 
     @BeforeEach
