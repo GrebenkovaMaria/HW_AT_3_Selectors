6ba0c95b8ee79dce04e81e376536e51c0fcbf6fc
Maria Grebenkova <grebenkova.m.p@gmail.com>
2021-04-13 23:19:01 +0300

    addArguments in setUp BeforeEach in test class

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -25,9 +25,13 @@ public class OrderTest {
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
         driver = new ChromeDriver();
     }
 
