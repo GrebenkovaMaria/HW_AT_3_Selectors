b26422dfd8634639c9ac3771552095119648de8d
Maria Grebenkova <grebenkova.m.p@gmail.com>
2021-04-18 21:27:49 +0300

    turn to Ubuntu in yml, adding driver for linux to repository

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,4 +1,3 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,7 +18,8 @@ public class OrderTest {
 
     @BeforeAll
         static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "\\webdriver\\chromedriver");
+        //WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
